b8a80349b66ba459331fc7243abb6f6a970872ed
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-03-27 00:14:22 +0700

    hehe

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
 
 mod index;
+mod predict;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,8 +11,12 @@ async fn main() -> std::io::Result<()> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     let server_address = format!("0.0.0.0:{}", port);
 
-    HttpServer::new(|| App::new().route("/", web::get().to(index::index)))
-        .bind(&server_address)?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index::index))
+            .route("/upload", web::post().to(predict::upload_file))
+    })
+    .bind(&server_address)?
+    .run()
+    .await
 }
--- src/predict.rs
+++ src/predict.rs
@@ -1,42 +1,26 @@
 use actix_multipart::Multipart;
-use actix_web::web;
+use actix_web::{web, Error, HttpResponse, Result};
 use futures::{StreamExt, TryStreamExt};
-use std::io::Write;
 
-#[derive(MultipartForm)]
-pub struct Upload {
-    file: TempFile,
-}
+pub async fn upload_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
+    let upload_dir = "./uploads";
+    std::fs::create_dir_all(upload_dir).ok();
 
-pub async fn put_file(config: web::Data<Config>, form: MultipartForm<Upload>) -> impl Responder {
-    const MAX_FILE_SIZE: u64 = 1024 * 1024 * 10; // 10 MB
-    const MAX_FILE_COUNT: i32 = 1;
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let file_path = format!("{}/uploaded_file.jpg", upload_dir);
+        let mut file_bytes = Vec::new();
 
-    // reject malformed requests
-    match form.file.size {
-        0 => return HttpResponse::BadRequest().finish(),
-        length if length > MAX_FILE_SIZE.try_into().unwrap() => {
-            return HttpResponse::BadRequest().body(format!(
-                "The uploaded file is too large. Maximum size is {} bytes.",
-                MAX_FILE_SIZE
-            ));
+        // Accumulate the chunks into a Vec<u8>
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            file_bytes.extend_from_slice(&data);
         }
-        _ => {}
-    };
 
-    let temp_file_path = form.file.file.path();
-    let file_name: &str = form
-        .file
-        .file_name
-        .as_ref()
-        .map(|m| m.as_ref())
-        .unwrap_or("null");
-
-    let mut file_path = PathBuf::from(&config.data_path);
-    file_path.push(&sanitize_filename::sanitize(&file_name));
-
-    match std::fs::rename(temp_file_path, file_path) {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
+        // Write the accumulated bytes to the file
+        let _ = web::block(move || std::fs::write(file_path, file_bytes)).await?;
     }
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body("File uploaded successfully"))
 }
